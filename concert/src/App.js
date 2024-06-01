ba4727b886c5b0077e494e7ca726507fbe23c43f
231008Jaco <231008@virtualwindow.co.za>
2024-06-01 14:00:24 +0200

    Backend
    Started setting up pages for backend.

--- concert/src/App.js
+++ concert/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Form } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './pages/Home';
 import CreateItem from './pages/CreateItem';
@@ -9,6 +9,8 @@ import EditItem from './pages/EditItem';
 import { TicketsContext } from './Components/TicketsContext';
 import { TicketsProvider } from './Components/TicketsContext';
 import './App.css';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
     return (
@@ -22,6 +24,8 @@ function App() {
                         <Route path="/list" element={<ItemList />} />
                         <Route path="/detail/:id" element={<ItemDetail />} />
                         <Route path="/edit" element={<EditItem />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/Login" element={<Login />} />
                     </Routes>
                 </div>
             </Router>
